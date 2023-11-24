27ad6f9bd60f1766e1e496189b1706fbc5269b8d
skyprogrammer <arkhamphhyre2@gmail.com>
2023-11-24 08:26:28 -0300

    main java plugin class defined

--- src/main/java/net/bypiramid/nonslipping/Main.java
+++ src/main/java/net/bypiramid/nonslipping/Main.java
@@ -1,23 +1,34 @@
 package net.bypiramid.nonslipping;
 
+import net.bypiramid.commandmanager.bukkit.BukkitFrame;
+import net.bypiramid.nonslipping.engine.manager.TrapManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
 
+    private BukkitFrame commandFrame;
+    private TrapManager trapManager;
+
     public Main() {
         instance = this;
     }
 
     @Override
-    public void onEnable() {
+    public void onLoad() {
+        commandFrame = new BukkitFrame(this);
+        trapManager = new TrapManager(this);
+    }
 
+    @Override
+    public void onEnable() {
+        commandFrame.registerCommands("net.bypiramid.commandmanager.nonslipping.command");
     }
 
     @Override
     public void onDisable() {
-
+        commandFrame = null;
     }
 
     public static Main getInstance() {
