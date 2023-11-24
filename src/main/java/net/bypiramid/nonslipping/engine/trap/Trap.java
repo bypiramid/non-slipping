4e31146ab2c312fb7d20c2ca01e51a636e709cd6
skyprogrammer <arkhamphhyre2@gmail.com>
2023-11-24 09:43:41 -0300

    now storing Player var

--- src/main/java/net/bypiramid/nonslipping/engine/trap/Trap.java
+++ src/main/java/net/bypiramid/nonslipping/engine/trap/Trap.java
@@ -7,21 +7,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.material.MaterialData;
 
-import java.util.UUID;
-
 public class Trap {
 
     public static final double MAX_DISTANCE_MOVABLE_INSIDE_TRAP = 0.637D;
     public static final double MAX_DISTANCE_MOVABLE_INSIDE_TRAP_WITH_BLOCK_IN_FACE = 0.9D;
 
-    private UUID uniqueId;
+    private Player player;
     private Location center;
     private MaterialData inputBlockData;
     private Block stuckBlock;
     private Cooldown teleportCooldown;
 
     public Trap(Player player, Location center, MaterialData inputBlockData, Block stuckBlock) {
-        this.uniqueId = player.getUniqueId();
+        this.player = player;
         this.center = center;
         this.inputBlockData = inputBlockData;
         this.stuckBlock = stuckBlock;
@@ -55,8 +53,12 @@ public class Trap {
         this.stuckBlock = stuckBlock;
     }
 
-    public boolean moveToCenter(Player target) {
-        if (!uniqueId.equals(target.getUniqueId())) {
+    public boolean exists() {
+        return false;
+    }
+
+    public boolean moveToCenter() {
+        if (!player.isOnline()) {
             return false;
         }
 
@@ -65,7 +67,7 @@ public class Trap {
         }
 
         teleportCooldown = new Cooldown(0.3);
-        target.teleport(center, PlayerTeleportEvent.TeleportCause.PLUGIN);
+        player.teleport(center, PlayerTeleportEvent.TeleportCause.PLUGIN);
         return false;
     }
 }
